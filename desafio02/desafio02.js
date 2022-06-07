58e5a16842891af79381b3f6695e32051742cd21
Wesley Monteiro <wesley.almont@gmail.com>
2022-06-07 12:59:52 -0300

    Solução de Desafio 02

---
+++ desafio02/desafio02.js
@@ -0,0 +1,21 @@
+"use strict";
+let pessoa1 = {
+    nome: "Maria",
+    idade: 29,
+    profissao: "Atriz"
+};
+let pessoa2 = {
+    nome: "Roberto",
+    idade: 19,
+    profissao: "Padeiro"
+};
+let pessoa3 = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+let pessoa4 = {
+    nome: "Carllos",
+    idade: 19,
+    profissao: "Padeiro"
+};
---
+++ desafio02/desafio02.ts
@@ -0,0 +1,29 @@
+interface Pessoa {
+  nome: string,
+  idade: number,
+  profissao: string,
+}
+
+let pessoa1: Pessoa = {
+  nome: "Maria",
+  idade: 29,
+  profissao: "Atriz"
+}
+
+let pessoa2: Pessoa = {
+  nome: "Roberto",
+  idade: 19,
+  profissao: "Padeiro"
+}
+
+let pessoa3: Pessoa = {
+  nome: "laura",
+  idade: 32,
+  profissao: "Atriz"
+}
+
+let pessoa4: Pessoa = {
+  nome: "Carllos",
+  idade: 19,
+  profissao: "Padeiro"
+}
