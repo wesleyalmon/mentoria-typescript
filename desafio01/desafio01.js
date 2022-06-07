0c81fe920bb37f84a4ec6507b49c93c010725c61
Wesley Monteiro <wesley.almont@gmail.com>
2022-06-07 12:54:11 -0300

    Solução de Desafio 01

---
+++ desafio01/desafio01.js
@@ -0,0 +1,7 @@
+"use strict";
+let employee = {
+    code: 10,
+    name: "John",
+};
+console.log(employee.name);
+console.log(employee.code);
---
+++ desafio01/desafio01.ts
@@ -0,0 +1,12 @@
+type Employee = {
+  code: number
+  name: string
+}
+
+let employee: Employee ={
+  code: 10,
+  name: "John",
+}
+
+console.log(employee.name)
+console.log(employee.code)
